50c168bbdae1ad31576b63fced9b89d8b70155e7
Diogo <diogorito137@gmail.com>
2024-01-10 17:42:28 +0000

    alterei o logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'ArealAuto',
   tagline: 'Os seus problemas são a nossa felicidade',
-  favicon: 'Auto Areal.png',
+  favicon: 'Auto-Areal.ico',
 
   // Set the production url of your site here
   url: 'https://github.com',
