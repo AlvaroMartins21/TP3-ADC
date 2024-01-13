7519c0f9377d8b3496f72554db90852bfa54ec8d
AlvaroMartins21 <alvaro.dc.martins@gmail.com>
2024-01-13 12:17:49 +0000

    icone

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'ArealAuto',
   tagline: 'Os seus problemas são a nossa felicidade',
-  favicon: 'Areal-Auto.ico',
+  favicon: 'favicon.ico',
 
   // Set the production url of your site here
   url: 'https://github.com',
@@ -63,12 +63,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'static/img/Areal Auto.jpg',
+      image: 'docusaurus.png',
       navbar: {
         title: 'Home',
         logo: {
           alt: 'My Site Logo',
-          src: 'static/img/Areal Auto.jpg',
+          src: 'docusaurus.png',
         },
         items: [
           {
